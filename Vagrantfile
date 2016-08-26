13f1c40eb20c233d7f0422b844d84df0179cfec7
Nathan Snyder <nsnyder@therestaurantstore.com>
2016-08-26 00:27:10 -0400

    Update to PHP7 and MariaDB

--- Vagrantfile
+++ Vagrantfile
@@ -69,6 +69,5 @@ Vagrant.configure(2) do |config|
   # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
   # documentation for more information about their specific syntax and use.
   config.vm.provision "shell", path: "provisioners/privileged.sh", privileged: true
-  config.vm.provision "shell", path: "provisioners/unprivileged.sh", privileged: false
 
 end
--- provisioners/privileged.sh
+++ provisioners/privileged.sh
@@ -1,10 +1,19 @@
+# Add an unofficial repository with PHP7 and other stuff
+sudo echo "deb http://packages.dotdeb.org jessie all" >> /etc/apt/sources.list
+
+wget https://www.dotdeb.org/dotdeb.gpg
+sudo apt-key add dotdeb.gpg
+
 sudo apt-get update
 
 # The string, "your_password" below should be replaced when setting up your own system
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password not_safe_password'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password not_safe_password'
 
-sudo apt-get install -y php5-common libapache2-mod-php5 php5-cli mysql-server php5-mysql
+#sudo apt-get install -y php5-common libapache2-mod-php5 php5-cli mariadb-server php5-mysql
+sudo apt-get install -y mariadb-server
+sudo apt-get install -y php7.0-common libapache2-mod-php7.0 php7.0-cli php7.0-mysql php-xml php7.0-mbstring
+
 
 sudo curl -sL https://deb.nodesource.com/setup_4.x | sudo bash -
 sudo apt-get install -y nodejs
@@ -25,5 +34,7 @@ sudo cp /var/www/html/test/provisioners/test.conf /etc/apache2/sites-available/t
 sudo ln /etc/apache2/sites-available/test.conf /etc/apache2/sites-enabled/test.conf
 # CHANGE THIS LINE #
 sudo cp /var/www/html/test/provisioners/envvars /etc/apache2/envvars
+
+composer install
 
 sudo systemctl restart apache2.service
