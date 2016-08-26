79798f5b948b13e42d22d7aa9520faf41b167808
Nathan Snyder <nsnyder@therestaurantstore.com>
2016-08-25 22:59:50 -0400

    Upgrade to Laravel 5.3

--- provisioners/privileged.sh
+++ provisioners/privileged.sh
@@ -6,6 +6,16 @@ sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again p
 
 sudo apt-get install -y php5-common libapache2-mod-php5 php5-cli mysql-server php5-mysql
 
+sudo curl -sL https://deb.nodesource.com/setup_4.x | sudo bash -
+sudo apt-get install -y nodejs
+
+sudo npm install -g gulp
+
+# CHANGE THIS LINE #
+cd /var/www/html/test
+
+npm install
+
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 # CHANGE THIS LINE #
 sudo cp /var/www/html/test/provisioners/apache2.conf /etc/apache2/apache2.conf
